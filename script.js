e0e1fd6dae6c99e5fff9e5e85ac8beaf4aa07469
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-23 10:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //complete this code
-class Person {}
+class Person {
+	Constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(name,"is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+		console.log(name,"is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
